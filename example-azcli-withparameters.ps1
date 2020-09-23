3b6f5f1ccf9e97f7a62a1b8fd79aabeac0ff0a40
Ironmanburns <36903988+Ironmanburns@users.noreply.github.com>
2020-09-23 14:22:38 +0100

    Create example-azcli-withparameters.ps1

---
+++ example-azcli-withparameters.ps1
@@ -0,0 +1,10 @@
+templateFile="{path-to-the-template-file}"
+devParameterFile="{path-to-azuredeploy.parameters.dev.json}"
+az group create \
+  --name myResourceGroupDev \
+  --location "East US"
+az deployment group create \
+  --name devenvironment \
+  --resource-group myResourceGroupDev \
+  --template-file $templateFile \
+  --parameters $devParameterFile
